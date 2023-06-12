724ee9f2237112c0189a95f20fb2e53322e74925
aalexebr <alex.ebr@icloud.com>
2023-06-12 14:04:04 +0200

    added discounts per age

--- js/script.js
+++ js/script.js
@@ -4,9 +4,16 @@ const userAge = prompt ("insert age");
 const km = parseInt(userKm)
 const age = parseInt(userAge)
 
-let totPrice = km * 0.21
+let totPrice = km * 0.1
+
+
+if (age <= 18) {
+    totPrice = totPrice - (totPrice * 0.2)
+}
+if (age >= 65) {
+    totPrice = totPrice - (totPrice * 0.4)
+}
+
+document.getElementById('total').innerHTML += "" + totPrice + "£"
 
 console.log('price', totPrice)
-
-document.getElementById('total').innerHTML = totPrice
-
