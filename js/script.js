346fde4cdd84ce064af504c06b42f018c11d3d11
aalexebr <alex.ebr@icloud.com>
2023-06-12 14:40:14 +0200

    added a table

--- js/script.js
+++ js/script.js
@@ -21,3 +21,24 @@ if (isNaN(userAge || userKm)){
     alert ("retard")
     document.getElementById('total').innerHTML = "retard"
 }
+
+
+//  table
+
+function add()
+         {
+           let number1, number2, priceTable, priceTable2;
+           number1 = parseInt(document.getElementById("firstnumber").value);
+           number2 = parseInt(document.getElementById("secondnumber").value);
+           priceTable = number1 * 0.21;
+           if (number2 < 18) {
+            priceTable = priceTable - (priceTable * 0.2)
+            }
+            if (number2 >= 65) {
+                priceTable = priceTable - (priceTable * 0.4)
+            }
+           priceTable2 = priceTable.toFixed(2)
+        
+           
+           document.getElementById('answer').innerHTML = priceTable2 + "$"
+         }
